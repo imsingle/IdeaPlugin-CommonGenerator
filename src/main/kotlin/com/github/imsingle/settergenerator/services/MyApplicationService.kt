a28cbef62f21233a925eb14d7351773ee48fe1e8
GitHub Action <action@github.com>
2022-06-06 08:19:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/imsingle/settergenerator/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/imsingle/settergenerator/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.imsingle.settergenerator.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.imsingle.settergenerator.MyBundle
 
 class MyApplicationService {
 
